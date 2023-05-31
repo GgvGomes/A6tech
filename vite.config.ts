e2d052a4c84cba62e7fc23a89821f7ce25aefb68
Gabriel Gomes <ggvgabriel05@gmail.com>
2023-05-31 13:35:23 -0300

    Projeto preparado.

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import tsconfigPaths from 'vite-tsconfig-paths';
 import eslintPlugin from 'vite-plugin-eslint';
-import * as path from 'path';
-import resolve from '@rollup/plugin-node-resolve';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslintPlugin()],
   resolve: {
     alias: [
-      // { find: '@features', replacement: path.resolve(__dirname, 'src/features') },
-      // { find: '@functions', replacement: path.resolve(__dirname, 'src/functions') },
-
-      // { find: '@' + /^(.*)\.js$/, replacement: '$1.alias' },
-      // { find: '@' + /^i18n\!(.*)/, replacement: '$1.js' }
       { find: /^(.*)\.js$/, replacement: '$1.alias' },
       { find: /^i18n\!(.*)/, replacement: '$1.js' },
 
